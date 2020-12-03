57f96607e33b83df9c7d5169f3b208488c75a1c6
rom4in <rom4in@me.com>
2020-12-03 08:24:51 +0000

    Initial Commit

---
+++ hover3d/hover3dApp.swift
@@ -0,0 +1,17 @@
+//
+//  hover3dApp.swift
+//  hover3d
+//
+//  Created by BigMac on 03/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct hover3dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
